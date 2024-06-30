a36d2a07e64c142eb2dcc2e9eef7415e97a94dcc
Ganesh Prabhakar mulik <129154209+ganesh-mulik@users.noreply.github.com>
2024-06-30 06:25:24 +0530

    first commit - component, services and interceptor initial structure created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {"path":"",redirectTo:"",pathMatch:"full"},
+    {"path":"",component:HomeComponent},
+    {"path":"my-profile",component:UserProfileComponent},
+    {"path":"contact-us",component:ContactUsComponent},
+];
